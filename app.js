1e0b38282ba3a5f61b863de70ba2678de42b8e1d
Luis Carlos Sarmiento <luisca033@hotmail.com>
2021-07-16 22:03:00 -0500

    final commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express =require("express") ;
 const rutas=require('./routes/routes')
 const app= express();
 app.use(rutas)
+
 app.listen(3000, ()=>{
     console.log('Arranco');
+    console.log('Nuevo clg'); 
 })
